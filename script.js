0775125da5e2cd9d6840ac0ce4382efac6e507b4
Morgane Jumel <53408984+MorganeJumel@users.noreply.github.com>
2020-10-24 14:37:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Burger menu //
+function onClickMenu() {
+    document.getElementById("burgerMenuBar").classList.toggle("change");
+    document.getElementById("navigation").classList.toggle("change");
+    document.getElementById("burgerMenuCircle").classList.toggle("changeCircle");
+}
+
+// Slider: la jauge qui indique où on en est dans la formation //
+const date1 = new Date();
+const date2 = new Date('9/11/2020');
+const diffTime = Math.abs(date2 - date1);
+const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+
+document.getElementById("myRange").value = 272 - diffDays; 
